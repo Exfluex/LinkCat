cea1a5b07aa6116734db6bae0c60eb6a1fd71d4d
Exfluex <exfluex@foxmail.com>
2022-02-16 17:41:28 +0800

    feat(render): Add card component for Card Render Module

---
+++ libs/card-render-component/src/lib/card/card.tsx
@@ -0,0 +1,14 @@
+import './card.module.scss';
+
+/* eslint-disable-next-line */
+export interface CardProps {}
+
+export function Card(props: CardProps) {
+  return (
+    <div>
+      <h1>Welcome to Card!</h1>
+    </div>
+  );
+}
+
+export default Card;
