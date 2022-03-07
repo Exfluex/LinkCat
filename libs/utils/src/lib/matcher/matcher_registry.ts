d8d07d1c9cb22bcb4f479a6709703a79681dada6
Exfluex <exfluex@foxmail.com>
2022-03-07 15:16:36 +0800

    fix: Fix NumberedMatcher deps lint error

--- libs/utils/src/lib/matcher/matcher_registry.ts
+++ libs/utils/src/lib/matcher/matcher_registry.ts
@@ -2,8 +2,7 @@ import { DefaultNumberRegistry, Registry } from "../registry";
 import { Matcher, PriorityMatcher } from "./matcher";
 
 
-export interface NumberedMatcher extends PriorityMatcher{
-}
+export type NumberedMatcher = PriorityMatcher
 
 export interface MatcherRegistry<Target,Env,M extends PriorityMatcher<Target,Env>> extends Matcher<Target,Env>,Registry<number,M>{
 }
